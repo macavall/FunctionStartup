d78a2ac442104e13367b6c305cfbc3f33a684f69
Matthew <matt.cavallo56@gmail.com>
2022-06-10 11:44:30 -0500

    First

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Azure;
+
+[assembly: FunctionsStartup(typeof(StartupFA.Startup))]
+
+namespace StartupFA
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+
+            var storageConnectionString = Environment.GetEnvironmentVariable("storageConnectionString");
+
+            services.AddLogging();
+            services.AddAzureAppConfiguration();
+            services.AddAzureClients();
+        }
+    }
+}
