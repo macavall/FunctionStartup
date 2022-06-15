0fa0170d3755c966b8fe68dc05e5a9ce8c540892
Matthew <matt.cavallo56@gmail.com>
2022-06-15 15:29:13 -0500

    Completed

--- Function1.cs
+++ Function1.cs
@@ -1,16 +1,27 @@
 using System;
+using Azure.Storage.Queues;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Logging;
 
 namespace StartupFA
 {
-    public static class Function1
+    public class Function1
     {
+        private readonly QueueServiceClient _queueStorage;
+
+        public Function1(IAzureClientFactory<QueueServiceClient> clientFactory)
+        {
+            _queueStorage = clientFactory.CreateClient("PrivateQueue");
+        }
+
         [FunctionName("Function1")]
         public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            _queueStorage.
         }
     }
 }
